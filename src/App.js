34c235165d81f2375906c062e018edd46e6e6bea
Washington Alexandre da Silva <washington.silva@totvs.com.br>
2020-01-28 19:03:29 -0300

    ESTRUTURAL: ajustado estrutura de pastas e arquivos

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+    <Text>Inicial</Text>
+    </>
+  );
+};
+
+export default App;
